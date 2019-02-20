597192434b6415dc4fbeb4170a420f85bf3924c9
Cancain <eriksson.tomas@gmail.com>
2019-02-20 14:21:22 +0100

    completed lecture 188

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,22 +5,39 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import post from '../../components/Post/Post';
 
 class Blog extends Component {
+
+    state = {
+        posts: []
+    }
+
     componentDidMount() {
         axios.get('http://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                console.log(response);
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(posts => {
+                    return {
+                        ...posts,
+                        author: 'Dowie'
+                    }
+                })
+                this.setState({ posts: updatedPosts });
+                // console.log(response);
             });
     }
 
     render() {
+
+        const posts = this.state.posts.map(posts => {
+            return <Post key={posts.id} title={posts.title} author={post.author} />
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
